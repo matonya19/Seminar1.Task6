00dd47f1ca92a6d149491368b658bb1cf2287e94
matonya <matonya19@mail.ru>
2022-12-05 11:07:20 +1000

    add task6

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N % 2 == 0)
+{
+    Console.WriteLine("Четное число");
+}
+else {
+    Console.WriteLine("Нечетное число");
+}
+
